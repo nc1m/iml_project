f9dc0860e81963ef0933d060e4d0ec3fa727940d
FlorianL314 <86008068+FlorianL314@users.noreply.github.com>
2022-02-14 04:02:47 +0100

    Update baselines.py

--- src/baselines.py
+++ src/baselines.py
@@ -1,25 +1,26 @@
-from lib2to3.pgen2 import token
 from typing import List
 import numpy as np
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-#from torchvision.transforms.functional import gaussian_blur
-
+from torchvision.transforms.functional import gaussian_blur
+from tokenizers import decoders
 
 # Maximum distance baseline = Choose word embedding which is most far away (L1-wise) from the input
 # Blurred baseline = Blurres out feature informations
 # Uniform baseline = Sample a random uniform embedding
-# Gaussian baseline = Sample from a gaussian around original embedding with some std. sigma (hyperparameter)
+# Gaussian baseline = Sample from a gaussian around original embedding
+# with some std. sigma (hyperparameter)
+
 
 BASELINE_TYPES = ['constant', 'maxDist', 'blurred', 'uniform', 'gaussian']
 
 
 
 class Baseline:
-    def __init__(self,modelName):
+    def __init__(self,model_name):
 
-        self.model_ = AutoModelForSequenceClassification.from_pretrained(modelName)
-        self.tokenizer_ = AutoTokenizer.from_pretrained(modelName)
+        self.model_ = AutoModelForSequenceClassification.from_pretrained(model_name)
+        self.tokenizer_ = AutoTokenizer.from_pretrained(model_name)
 
         self.full_embs = self.model_.get_input_embeddings().weight.detach().clone()
         self.voc = self.tokenizer_.get_vocab()
@@ -29,7 +30,7 @@ class Baseline:
             self.std = torch.std(self.full_embs)
             self.lowest_emb_val =  self.full_embs.min(0).values
             self.highest_emb_val = self.full_embs.max(0).values
-            self.between = (self.highest_emb_val - self.lowest_emb_val ) / 2
+            self.between = (self.highest_emb_val + self.lowest_emb_val ) / 2
 
     def baseline_by_type(self,baseline_type : str,input : str):
         """Return a list baseline tokens
@@ -190,23 +191,27 @@ class Baseline:
 model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-# print(tokenizer.decode(base.maximum_distance_embedding("I love you!")))
+
+# print(' '.join(tokenizer.decode(base.maximum_distance_embedding("I really hate you"))))
+
 # print(tokenizer.decode(base.gaussian_embedding("I love you!")))
 # print(tokenizer.decode(base.uniform_embedding("I love you!")))
 
 
-
-# voc = tokenizer.get_vocab()
 # full_emb = model.get_input_embeddings().weight.detach().clone()
 # mean = torch.mean(full_emb)
 # va = torch.std(full_emb)
-# enc = tokenizer.encode("House")[1]
-# emb = model.get_input_embeddings()(torch.tensor(enc))
-
 # samp = torch.Tensor(np.random.normal(mean,va,size=full_emb.shape[1]))
-# print(samp.shape)
-#print(emb.unsqueeze(0).shape)
-# emb = gaussian_blur(emb.unsqueeze(0))
+
+voc = tokenizer.get_vocab()
+
+enc = tokenizer.encode("House")[1]
+emb = model.get_input_embeddings()(torch.tensor(enc)).detach().clone()
+
+
+
+emb = gaussian_blur(emb.ToTensor(),kernel_size=[1,3])
+print(emb)
 
 
 # print(emb)
