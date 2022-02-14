024167e7c6fde466db9a5c08011918d10bf936b4
trihiepbui <hiepbui945@gmail.com>
2022-02-14 20:48:02 +0100

    dig files to src

--- src/dig.py
+++ src/dig.py
@@ -13,6 +13,7 @@ from captum.attr._utils.batching import _batch_attribution
 from captum.attr._utils.common import _format_input_baseline, _reshape_and_sum, _validate_input, _format_input
 
 
+
 class DiscretetizedIntegratedGradients(GradientAttribution):
 
 	def __init__(self, forward_func: Callable, multiply_by_inputs: bool = True) -> None:
@@ -25,14 +26,18 @@ class DiscretetizedIntegratedGradients(GradientAttribution):
 		scaled_features: Tuple[Tensor, ...],
 		target: TargetType = None,
 		additional_forward_args: Any = None,
-		n_steps: int = 50,
+		n_steps: int = 10,
 		return_convergence_delta: bool = False,
 	) -> Union[
 		TensorOrTupleOfTensorsGeneric, Tuple[TensorOrTupleOfTensorsGeneric, Tensor]
 	]:
 		is_inputs_tuple		= _is_tuple(scaled_features)
 		scaled_features_tpl	= _format_input(scaled_features)
-		attributions		= self.calculate_dig_attributions(scaled_features_tpl=scaled_features_tpl, target=target, additional_forward_args=additional_forward_args, n_steps=n_steps)
+		#scaled_features_tpl = scaled_features
+	
+		
+		attributions, interpolated_input, gradientsAt_interpolation, cummulative_gradients = self.calculate_dig_attributions(scaled_features_tpl=scaled_features_tpl, target=target, additional_forward_args=additional_forward_args, n_steps=n_steps)
+
 		if return_convergence_delta:
 			assert len(scaled_features_tpl) == 1, 'More than one tuple not supported in this code!'
 			start_point, end_point = _format_input(scaled_features_tpl[0][0].unsqueeze(0)), _format_input(scaled_features_tpl[0][-1].unsqueeze(0))	# baselines, inputs (only works for one input, i.e. len(tuple) == 1)
@@ -44,16 +49,17 @@ class DiscretetizedIntegratedGradients(GradientAttribution):
 				additional_forward_args=additional_forward_args,
 				target=target,
 			)
-			return _format_output(is_inputs_tuple, attributions), delta
+			return _format_output(is_inputs_tuple, attributions), delta, interpolated_input, gradientsAt_interpolation, cummulative_gradients
 
-		return _format_output(is_inputs_tuple, attributions)
+		return _format_output(is_inputs_tuple, attributions), interpolated_input, gradientsAt_interpolation, cummulative_gradients
+
 
 	def calculate_dig_attributions(
 		self,
 		scaled_features_tpl: Tuple[Tensor, ...],
 		target: TargetType = None,
 		additional_forward_args: Any = None,
-		n_steps: int = 50,
+		n_steps: int = 50,#50
 	) -> Tuple[Tensor, ...]:
 		additional_forward_args = _format_additional_forward_args(additional_forward_args)
 		input_additional_args	= (_expand_additional_forward_args(additional_forward_args, n_steps) if additional_forward_args is not None else None)
@@ -66,13 +72,16 @@ class DiscretetizedIntegratedGradients(GradientAttribution):
 			target_ind=expanded_target,
 			additional_forward_args=input_additional_args,
 		)
-		
+
 		# calculate (x - x') for each interpolated point
 		shifted_inputs_tpl	= tuple(torch.cat([scaled_features[1:], scaled_features[-1].unsqueeze(0)]) for scaled_features in scaled_features_tpl)
 		steps				= tuple(shifted_inputs_tpl[i] - scaled_features_tpl[i] for i in range(len(shifted_inputs_tpl)))
 		scaled_grads		= tuple(grads[i] * steps[i] for i in range(len(grads)))
 
+		cummulative_gradients = tuple(_reshape_and_sum(scaled_grad, 1, grad.shape[0] // 1, grad.shape[1:]) for (scaled_grad, grad) in zip(scaled_grads, grads))
+
 		# aggregates across all steps for each tensor in the input tuple
 		attributions		= tuple(_reshape_and_sum(scaled_grad, n_steps, grad.shape[0] // n_steps, grad.shape[1:]) for (scaled_grad, grad) in zip(scaled_grads, grads))
 
-		return attributions, None, None, None
+		#return attributions
+		return attributions,  scaled_features_tpl[0], scaled_grads[0], cummulative_gradients[0]
